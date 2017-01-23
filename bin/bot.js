44f043802e079ddd787192de9644f67391ef4a75
dloeda <diego.loeda@gmail.com>
2017-01-23 17:19:04 +0100

    🎨  refactor(js): Refactor of libs to allow dependency injection

--- bin/bot.js
+++ bin/bot.js
@@ -1,14 +1,24 @@
 'use strict'
 
-let botkit = require('botkit')
-let bokzuy = require('../lib/bokzuy-actions')
+const botkit = require('botkit')
+const http = require('../lib/http-utils')
+const logger = require('../lib/logger').init()
+const request = require('request')
+const bokzuy = require('../lib/bokzuy-actions')
 
-let token = process.env.slack_bokzuy_token;
-let controller = botkit.slackbot({
+const endpoint = 'https://api.bokzuy.com'
+const token = process.env.slack_bokzuy_token
+
+const controller = botkit.slackbot({
     debug: true
 })
 
-bokzuy.init()
+bokzuy.init({
+    endpoint,
+    http,
+    logger,
+    request
+})
 
 controller.setupWebserver(3000, (err, webserver) => controller.createWebhookEndpoints(webserver))
 
@@ -16,8 +26,8 @@ controller.on('slash_command', (bot, message) => {
     if(message.token === token) {
         if (message.command === '/badge') {
             // process message
-            bokzuy.sendBadge({}, (error, response) => {
-                if (error) Bok.replyPublic(message, 'Some kind of magic is breaking our servers, try gain later')
+            bokzuy.sendBadge({}, (error) => {
+                if (error) bot.replyPublic(message, 'Some kind of magic is breaking our servers, try gain later')
                 else bot.replyPublic(message, 'Badge sent, no regrets')
             })
         } else {
