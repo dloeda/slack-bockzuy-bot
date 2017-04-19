cea039d2da5a52751ba62658af630afd80b40959
dloeda <diego.loeda@gmail.com>
2017-04-19 15:57:09 +0200

    ✨  feat: Parse register action

--- bin/bot.js
+++ bin/bot.js
@@ -27,17 +27,27 @@ controller.setupWebserver(3000, (err, webserver) => controller.createWebhookEndp
 controller.on('slash_command', (bot, message) => {
     if(message.token === token) {
         if (message.command === '/badge') {
-            // process message
             var parsedMessage = message.replace(/\\badge\ ([\w]+)\ ([\w]+)\ ([\w]+)?\ (\".+\")/, '$1,$2,$3,$4').split(',')
-            bokzuy.sendBadge({
-                name: parsedMessage[0],
-                badge: parsedMessage[1],
-                group: parsedMessage[2],
-                comment: parsedMessage[3]
-            }, (error) => {
-                if (error) bot.replyPublic(message, 'Some kind of magic is breaking our servers, try gain later')
-                else bot.replyPublic(message, 'Badge sent, no regrets')
-            })
+            if (/^\/badge\ register/.test(message.text)) {
+                bokzuy.registerUser({
+                    user: parsedMessage[1],
+                    pass: parsedMessage[2]
+                }, (error) => {
+                    if (error) bot.replyPublic(message, 'Some kind of magic is breaking our servers, try gain later')
+                    else bot.replyPublic(message, 'User registered, send a badge now!')
+                })
+            } else {
+                // process message
+                bokzuy.sendBadge({
+                    name: parsedMessage[0],
+                    badge: parsedMessage[1],
+                    group: parsedMessage[2],
+                    comment: parsedMessage[3]
+                }, (error) => {
+                    if (error) bot.replyPublic(message, 'Some kind of magic is breaking our servers, try gain later')
+                    else bot.replyPublic(message, 'Badge sent, no regrets')
+                })
+            }
         } else {
             bot.replyPrivate(message, 'I find this command... <https://media.giphy.com/media/3oAt21Fnr4i54uK8vK/giphy.gif>')
         }
